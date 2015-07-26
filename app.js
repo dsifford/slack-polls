9421f08bc7f7c61758f7ee8f3aedc721fe7bb814
dsifford <dereksifford@gmail.com>
2015-07-25 23:54:20 -0400

    Begin handler functions

--- app.js
+++ app.js
@@ -8,54 +8,122 @@ var express     = require('express'),
 // TESTING VARIABLES
 var PORT = 3000;
 
-// Connect to MongoDB
-
-
-
 // Include the bodyParser Middleman + allow x-url-encoding
 // to be extended to the body of the request
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+/**
+ * Global MongoDB URI
+ * @type {String}
+ */
+var MONGODB_URI = 'mongodb://localhost:27017/slackpolls';
+var db;
+
+
+////////////////////////
+// Connect to MongoDB //
+////////////////////////
+
+MongoClient.connect(MONGODB_URI, function(err, database) {
+    assert.equal(err, null);
+    console.log("Connected correctly to server");
+
+    db = database;
+
+    app.listen(process.env.PORT || PORT);
+    console.log('App listening on http://localhost/%s', PORT);
+});
+
+///////////////////////////////
+// POST Route for index path //
+///////////////////////////////
 
 app.post('/', function(req, res, next) {
     console.log("POST request received at '/'.");
 
     // Collect request and set variables
-    var name = req.body.user_name;
-    var domain = req.body.team_domain;
-    var pollMethod = req.body.text.match(/\S*/)[0].toLowerCase();
-    var pollParams = req.body.text.substr(pollMethod.length).trim();
-    var channelId = req.body.channel_id;
+    var input = {
+        name: req.body.user_name,
+        domain: req.body.team_domain,
+        pollMethod: req.body.text.match(/\S*/)[0].toLowerCase(),
+        pollParams: function() { return req.body.text.substr(input.pollMethod.length).trim(); },
+        channelId: req.body.channel_id
+    };
+    console.log(input.channelId);
 
     // Set output options TODO: Setup /poll {setup} to save token in database
     var options = {
         method: 'POST',
-        url: 'https://' + domain + '.slack.com/services/hooks/slackbot',
-        qs: { token: '80Q9s54PMHVjOXgJE1CKlzVf', channel: channelId },
+        url: 'https://' + input.domain + '.slack.com/services/hooks/slackbot',
+        qs: { token: '80Q9s54PMHVjOXgJE1CKlzVf', channel: input.channelId },
         body: ''
     };
 
-    // Output logic
-    if (pollMethod == 'new') {
-        // Run 'new' handler
-    } else if (pollMethod == 'vote') {
+    //////////////////
+    // Output logic //
+    //////////////////
+
+    if (input.pollMethod == 'new') {
+        removePoll(input);
+        newPoll(input);
+    } else if (input.pollMethod == 'vote') {
         // Run 'vote' handler
-    } else if (pollMethod == 'help') {
+    } else if (input.pollMethod == 'help') {
         help(options);
-    } else if (pollMethod == 'results') {
+    } else if (input.pollMethod == 'results') {
         // Run 'results' handler
     }
 
     console.log('> Ending Response...');
     res.end();
-});
 
-app.listen(process.env.PORT || PORT, function() {
-    console.log('App listening on http://localhost/%s', PORT);
 });
 
 
-// Handler function for /poll {new}
-    // TODO:
+///////////////////////////////////////////////////////////
+// --------------------- FUNCTIONS --------------------- //
+///////////////////////////////////////////////////////////
+
+// Handler functions for /poll {new}
+
+    var removePoll = function(input) {
+        db.collection("polls").deleteOne(
+            { "pollId": input.channelId },
+            function(err, results) {
+                console.log(results);
+            }
+        );
+    };
+
+    // TODO: Add 'poll graphic' array to this collection
+    var newPoll = function(input) {
+        db.collection("polls").insertOne( {
+            "pollId" : input.channelId,
+            "question" : "NEW POLL INSERTED! YAYYY!!!!",
+            "answers" : {
+                "A" : {
+                    "choice" : "This is choice A",
+                    "score" : 0
+                },
+                "B" : {
+                    "choice" : "This is choice B",
+                    "score" : 0
+                },
+                "C" : {
+                    "choice" : "This is choice C",
+                    "score" : 0
+                },
+                "D" : {
+                    "choice" : "This is choice A",
+                    "score" : 0
+                }
+            }
+        }, function(err, result) {
+            assert.equal(err, null);
+            console.log("Inserted a new poll into the polls collection.");
+        });
+    };
 
 // Handler function for /poll {vote}
     // TODO:
